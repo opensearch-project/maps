8b52ab5089aba1311bc3a035106233bed25bd806
Junqiu Lei <junqiu@amazon.com>
2022-06-17 09:03:32 -0700

    Update entrypoint.sh
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- tiles-generation/docker/vector-tile/entrypoint.sh
+++ tiles-generation/docker/vector-tile/entrypoint.sh
@@ -3,41 +3,72 @@
 #  Copyright OpenSearch Contributors
 #  SPDX-License-Identifier: Apache-2.0
 
+set -e
+
+usage() {
+    echo ""
+    echo "This script is used to build map vector tiles to pbf files and upload to s3 bucket."
+    echo "For planet setting, reference to https://github.com/onthegomap/planetiler/blob/main/PLANET.md"
+    echo "--------------------------------------------------------------------------"
+    echo ""
+    echo "docker environment variables:"
+    echo -e "AREA                  \tSpecify the area that we want to generate, for planet, it's 'planet', other areas name reference to https://download.geofabrik.de/"
+    echo -e "TILE_S3_BUCKET        \tSpecify the S3 bucket to store the generated vector pbf tiles"
+    echo -e "AWS_ACCESS_KEY_ID     \tSpecify the AWS access key id used used for s3 copy"
+    echo -e "AWS_SECRET_ACCESS_KEY \tSpecify the AWS AWS secret access key used for s3 copy"
+    echo -e "JAVA_TOOL_OPTIONS     \tSpecify the JVM config, for example:'-Xmx1g'"
+    echo -e "STORAGE               \t(Needed for planet tile generation)Specify node location cache in memory by 'ram' or in a temporary memory-mapped file by 'mmap'"
+    echo ""
+    echo "--------------------------------------------------------------------------"
+}
+
+# Validate the required parameters to present
+validateParameter() {
+    if [ -z "$AREA" ] || [ -z "$TILE_S3_BUCKET" ] || [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+      echo "You must specify '-e AREA', '-e TILE_S3_BUCKET', '-e AWS_ACCESS_KEY_ID', '-e AWS_SECRET_ACCESS_KEY' in container environment variables"
+      usage
+      exit 1
+    fi
+}
+
 if [ "$1" == "generate" ]; then
-    # Generate mbtiles
-    if [ $AREA -eq "planet" ]
+    validateParameter
+
+    if [ "$AREA" == "planet" ]
       then
-        # https://github.com/onthegomap/planetiler/blob/main/PLANET.md
-        java -Xmx${RAM} \
-              # return unused heap memory to the OS
-              -XX:MaxHeapFreeRatio=40 \
+        java "${JAVA_TOOL_OPTIONS}" \
               -jar planetiler.jar \
               # Download the latest planet.osm.pbf from s3://osm-pds bucket
               --area=planet --bounds=planet --download \
               # Accelerate the download by fetching the 10 1GB chunks at a time in parallel
               --download-threads=10 --download-chunk-size-mb=1000 \
-              # Also download name translations from wikidata
+              # Download name translations from wikidata
               --fetch-wikidata \
-              --mbtiles=output.mbtiles \
+              --mbtiles="${AREA}".mbtiles \
               # Store temporary node locations in memory
-              --nodemap-type=array --storage=${STORAGE} \
+              --nodemap-type=array --storage="${STORAGE}" \
               # compact_db is not compatible with mbutil, set it to false
               --compact_db=false
         else
-          java -Xmx${RAM} \
+          java  "${JAVA_TOOL_OPTIONS}" \
                 -jar planetiler.jar \
                 --download \
-                --area=${AREA} \
-                --mbtiles=output.mbtiles \
+                --area="${AREA}" \
+                --mbtiles="${AREA}".mbtiles \
                 --compact_db=false
     fi
 
     # Extract mbtiles to zxy pbf tiles
-    mb-util output.mbtiles tiles --image_format=pbf
+    mb-util "${AREA}".mbtiles tiles --image_format=pbf
 
     # Upload to S3 Bucket
     currentTime=$(date "+%Y%m%d%H%M%S")
-    /s5cmd cp tiles/ s3://${TILE_S3_BUCKET}/${currentTime}/vector/
+    /s5cmd cp tiles/ s3://"${TILE_S3_BUCKET}"/"${currentTime}"/vector/
+    exit 0
+fi
+
+if [ "$1" == "help" ]; then
+    usage
     exit 0
 fi
 
